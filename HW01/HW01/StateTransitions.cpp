69b5297c9b3888dce92db84edf2d1d7f00b83e60
William Gillespie <wgilles1@emich.edu>
2016-09-27 21:36:24 -0400

    Initial commit of project files and the testing of the hashmap.  Also, a gitignore file is created for xcode, c++, and python.

---
+++ HW01/HW01/StateTransitions.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW01
+//
+//  Created by William Gillespie on 9/27/16.
+//  Copyright © 2016 William Gillespie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
