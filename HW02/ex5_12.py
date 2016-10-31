b3a3d3116efb6d3cf1053e79d532565ea807d20d
William Gillespie <wgilles1@emich.edu>
2016-10-30 21:06:15 -0400

    Added to the gitignore to ignore files that pycharm creates. I also added ex5_12.py.  This is exercise 5.12.

---
+++ HW02/ex5_12.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import numpy as np
+"""
+exercise 5.12
+Study the asymptotic behavior of the following three-dimensional
+difference equation model by calculating its eigenvalues and eigenvectors
+"""
+print("hello")
