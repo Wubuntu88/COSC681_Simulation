e34311ec7c7a06371568910512ee569e44a1d234
William Gillespie <wgilles1@emich.edu>
2016-11-02 12:22:00 -0400

    It works and gives a pretty picture, but the derivative part is cheating.  This is because if the resource dips below 0 it is put back to zero.

--- HW02/ex6_8.py
+++ HW02/ex6_8.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import numpy as np
+
 """
 The resource will have a logistic growth model
 The resource is represented as x; The change at each step is the following:
@@ -21,23 +23,35 @@ zombies = 2.0
 resources_pop = []
 zombie_goosies_pop = []
 zombies_pop = []
-
-time_range = np.arange(start=0, stop=900 + Dt, step=Dt)
+stop_limit = 1300
+time_range = np.arange(start=0, stop=stop_limit + Dt, step=Dt)
 for t in time_range:
     resources_pop.append(resource)
     zombies_pop.append(zombies)
     zombie_goosies_pop.append(zombie_goosies)
 
-    #resource += r * resource * (1 - resource / 200) * Dt \
-    resource += 30 \
-                - .1 * zombies \
-                - .05 * zombie_goosies
-    zombies += .001 * resource - .01
-    zombie_goosies += .002 * resource - .03
+    # resource += r * resource * (1 - resource / 200) * Dt \
+    resource += 30 - \
+        (.1 * zombies + .05 * zombie_goosies)
+    if resource < 0:
+        resource = 0
+    zombies += .001 * resource - .7
+    zombie_goosies += .002 * resource - 1.5
     print("change: " + str(r * resource * (1 - resource / 200) * Dt))
 
-plt.plot(time_range, resources_pop)
-plt.plot(time_range, zombies_pop)
-plt.plot(time_range, zombie_goosies_pop)
-plt.show()
+plt.plot(time_range, resources_pop, linewidth=3, c='green')
+plt.plot(time_range, zombies_pop, linewidth=3, c='orange')
+plt.plot(time_range, zombie_goosies_pop, linewidth=3, c='red')
 
+# labels & titles
+plt.title("Two species competing for a resource", fontsize=20)
+plt.xlabel("Time", fontsize=18)
+plt.ylabel("Population", fontsize=18)
+
+# legend
+resource_patch = mpatches.Patch(color='green', label='Resource')
+zombies_patch = mpatches.Patch(color='orange', label='Zombies')
+zombies_goosies_patch = mpatches.Patch(color='red', label='Zombie goosies')
+plt.legend(handles=[resource_patch, zombies_patch, zombies_goosies_patch], loc=1)
+
+plt.show()
