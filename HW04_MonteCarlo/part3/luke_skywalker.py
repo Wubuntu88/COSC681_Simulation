01679f5f3ed750f818931fcb7d32e97ef952f89f
William Gillespie <wgilles1@emich.edu>
2016-12-15 14:42:34 -0500

    Now It has the plot commented out. It also calculates the number of shots Luke has to make to have a 75% chance or greater of hitting the target.

--- HW04_MonteCarlo/part3/luke_skywalker.py
+++ HW04_MonteCarlo/part3/luke_skywalker.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
+import math as math
 import numpy as np
 import random as random
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import seaborn as sns
 
 
@@ -54,17 +56,21 @@ def simulate_shot():
         return (positions[0], positions[1]), True
 
 
-# Two tasks:
+# Three tasks:
 # 1) Calculate probability of success
 # 2) Plot figure of hits, and misses
-
+# 3) Answer this question:
+# How many shots will Luke have to take to have a 75% or
+# better chance of hitting the target?
 
 hits_xs = []
 hits_ys = []
 misses_xs = []
 misses_ys = []
 
-for iteration in range(0, 5000):
+ITERATIONS = 10000
+
+for iteration in range(0, ITERATIONS):
     result = simulate_shot()
     x, y = result[0]
     if result[1]:  # if result was a hit
@@ -77,15 +83,51 @@ for iteration in range(0, 5000):
 successes = len(hits_xs)
 failures = len(misses_xs)
 
+# Part 1
 probability_of_success = float(successes) / failures
+print("Part 1: Calculate probability of success")
 print("P(success): {0:.4f}".format(probability_of_success))
 
-'''
+''' #remove comments to plot the figure
+# Part 2
 plt.scatter(hits_xs, hits_ys, c="blue", s=30)
 plt.scatter(misses_xs, misses_ys, c="red", s=30)
 
-plt.title("Luke's shots at the bottom of the shaft", fontsize=20)
+red_patch = mpatches.Patch(color='red', label='Misses or Wall Collisions')
+blue_patch = mpatches.Patch(color='blue', label='Hits')
+
+
+plt.title("Luke's shots at the bottom of the shaft\n" +
+          "iterations: {:d}, hits: {:d}, misses: {:d}"
+          .format(ITERATIONS, len(hits_xs), len(misses_xs)),
+          fontsize=20)
 plt.xlabel("x", fontsize=16)
 plt.ylabel("y", fontsize=16)
 plt.show()
 '''
+
+# one output of program:
+# P(success): 0.0124
+
+
+# Part 3) Calculate the # of times luke must shoot to have a
+# 75% chance or better of hitting the target
+
+# to do this, we will calculate the probability of failure and rephrase the question
+# How many times do you have to shoot to have a 25% chance or lower of missing all of them
+# Equation steps:
+# A: 1 - P(failure) ^ n = .75
+# B: .25 = P(failure) ^ n
+# C: ln(.25) = ln(P(failure) ^ n)
+# D: ln(.25) = n * ln(P(failure))
+# E: ln(.25) / ln(P(failure)) = n
+# F: take the ceil of n; that is the number of times to shoot to have
+# a 75% chance or greater of shooting the target.
+probability_of_failure = 1 - probability_of_success
+n_shots = math.log(.25) / math.log(probability_of_failure)
+n_shots = int(math.ceil(n_shots))
+print("Number of shots to have a 75% chance or greater of hitting the target: {:d}"
+      .format(n_shots))
+# one output (for P(success) = 0.0124):
+# Number of shots to have a 75% chance or greater of hitting the target: 112
+
